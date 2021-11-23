12316ff560750a1ea49e88e57263d8f3c42fd758
Ernest Bieś <ernestbies@gmail.com>
2021-11-23 10:04:49 +0100

    Config update.

--- config.js
+++ config.js
@@ -4,9 +4,7 @@ const config = {
     databaseUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/personal_website',
     recaptchaSecretKey: process.env.RECAPTCHA_SECRET_KEY,
     service: process.env.SERVICE,
-    contactEmail: process.env.CONTACT_EMAIL,
-    emailAddress: process.env.EMAIL,
-    emailPassword: process.env.EMAIL_PASSWORD
+    contactEmail: process.env.CONTACT_EMAIL
 };
 
 export default config;

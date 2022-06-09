8fdf0b7e11d930063c51443db366667e6663a4af
Ernest Bieś <ernestbies@gmail.com>
2022-06-09 15:33:03 +0200

    Added PUT & GET by ID requests.

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,35 +1,62 @@
 import Post from '../models/post';
 import Stat from '../models/stat';
-import postValidation from "../utils/postValidationSchema";
-import convert from "../services/mongoConverter";
+import postValidation from '../utils/postValidationSchema';
+import convert from '../services/mongoConverter';
 
 const postController = {
+    getPost: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const post = await Post.findById(id);
+            if (post) {
+                res.status(200).send(convert(post));
+            } else {
+                res.status(404).send({
+                    errorCode: 404,
+                    errorMessage: 'Post not found.',
+                });
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
     getAllPosts: async (req, res) => {
         try {
             const posts = await Post.find();
             const stats = await Stat.find();
             let newStats = {};
 
-            if(!stats.length) {
-                await new Stat({visitors: 0}).save();
+            if (!stats.length) {
+                await new Stat({ visitors: 0 }).save();
             } else {
-                newStats = await Stat.findByIdAndUpdate(stats[0]._id, {$inc: {visitors : 1}});
+                newStats = await Stat.findByIdAndUpdate(stats[0]._id, {
+                    $inc: { visitors: 1 },
+                });
             }
 
-            res.status(200).send({visitors: newStats.visitors, posts: convert(posts)});
+            res.status(200).send({
+                visitors: newStats.visitors,
+                posts: convert(posts),
+            });
         } catch (error) {
-            res.status(500).send(error)
+            res.status(500).send(error);
         }
     },
     createPost: async (req, res) => {
-        const {body} = req;
-        const users = await Post.find({username: body.username});
-        if(users.length) {
-            res.status(401).send({errorCode: 401, errorMessage: 'Username already taken.'});
+        const { body } = req;
+        const users = await Post.find({ username: body.username });
+        if (users.length) {
+            res.status(401).send({
+                errorCode: 401,
+                errorMessage: 'Username already taken.',
+            });
         } else {
-            const {error} = postValidation(body, 'post');
+            const { error } = postValidation(body, 'post');
             if (error) {
-                res.status(401).send({errorCode: 401, errorMessage: error.details[0].message});
+                res.status(401).send({
+                    errorCode: 401,
+                    errorMessage: error.details[0].message,
+                });
             } else {
                 const newPost = new Post(body);
                 try {
@@ -45,16 +72,24 @@ const postController = {
     updatePost: async (req, res) => {
         const { id } = req.params;
         const { body } = req;
-        const post = await Post.exists({_id: id});
-        if(!post) {
-            res.status(401).send({errorCode: 401, errorMessage: 'Post not found'});
+        const post = await Post.exists({ _id: id });
+        if (!post) {
+            res.status(401).send({
+                errorCode: 401,
+                errorMessage: 'Post not found.',
+            });
         } else {
-            const {error} = postValidation(body, 'patch');
+            const { error } = postValidation(body, 'put');
             if (error) {
-                res.status(401).send({errorCode: 401, errorMessage: error.details[0].message});
+                res.status(401).send({
+                    errorCode: 401,
+                    errorMessage: error.details[0].message,
+                });
             } else {
-                const newPost = await Post.findOneAndUpdate({_id: id}, body, {new: true});
-                if(!newPost) {
+                const newPost = await Post.findOneAndUpdate({ _id: id }, body, {
+                    new: true,
+                });
+                if (!newPost) {
                     res.status(500).send('Cannot update post.');
                 } else {
                     res.status(200).send(convert(newPost));
@@ -68,11 +103,11 @@ const postController = {
         const removedPost = await Post.findByIdAndRemove(id);
 
         if (!removedPost) {
-            res.status(400).send('Could not delete post');
+            res.status(400).send('Could not delete post.');
         } else {
-            res.status(204).send('Deleted post successfully');
+            res.status(204).send('Deleted post successfully.');
         }
-    }
+    },
 };
 
 export default postController;
--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,7 @@ import postController from '../controllers/postController';
 const router = express.Router();
 
 router.get('/posts', postController.getAllPosts);
+router.get('/posts/:id', postController.getPost);
 router.post('/posts', postController.createPost);
 // router.put('/posts/:id', postController.updatePost);
 // router.delete('/posts/:id', postController.deletePost);
